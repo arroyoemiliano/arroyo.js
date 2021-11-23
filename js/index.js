dc9ae2a3a8cad1864940bc06a2822fa5ca9d9227
arroyoemiliano <arroyoemiliano@users.noreply.github.com>
2021-11-22 21:59:18 -0300

    Primer deploy practico 1

---
+++ js/index.js
@@ -0,0 +1,64 @@
+
+
+
+/*
+//Pedir número mediante prompt y si es mayor a 1000 mostrar un alert.
+let numero = +prompt('Ingresa un Numero')
+
+if (!numero == Number || numero < 1000) {
+    alert('NO es un numero mas alto que 1000')
+}else{
+    alert('Mayor a 1000')
+}
+*/
+
+
+
+
+//Pedir un texto mediante prompt, y si es igual a "Hola" mostrar un alerta por consola.
+// let nombre = prompt('¿Indicanos tu nombre?')
+// nombre = nombre.toLocaleUpperCase
+
+// console.log(nombre, typeof nombre)
+
+
+// if (!nombre == 'HOLA' || nombre === '') {
+//     alert('No dijiste hola, Chau!')
+    
+// }else{
+//     alert('Hola amigo')
+// }
+
+
+
+// console.log(edad, typeof edad)
+
+
+//Pedir un número por prompt y evaluar si está entre 10 y 50. En caso positivo mostrar un alert.
+let edad =  +prompt('¿Cual es tu edad?')
+
+if (edad <= 10 || edad >= 50) {
+    alert('Pusiste un numero POR FUERA DE 10 y 50')
+}else{
+    alert('Pusiste un numero entre 10 y 50')
+}
+
+
+
+
+
+/*  
+Condicionales
+
+        <
+        <=
+        >
+        >=
+        ||
+        &&
+        !
+        ===
+        !=
+
+*/
+
